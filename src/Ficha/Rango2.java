6373477f9e9b586638da8d092307f4ea1f483bbf
JoseLuisDavid19 <Joseluisyoda@gmail.com>
2016-11-17 16:00:45 -0600

    ficha

---
+++ src/Ficha/Rango2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ficha;
+
+/**
+ *
+ * @author JoseLuis
+ */
+public class Rango2  extends ficha{
+    public Rango2(String nombre,String tipo,String bando,int rango){
+    super(nombre,rango,tipo,bando);
+    }
+
+    @Override
+    public void mover(String arg[]) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
