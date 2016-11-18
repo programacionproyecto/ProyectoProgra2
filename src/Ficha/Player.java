3c130eb3dea7df3307c532ffef15fbbaa512542d
JoseLuisDavid19 <Joseluisyoda@gmail.com>
2016-11-18 15:23:58 -0600

    player

--- src/Ficha/Player.java
+++ src/Ficha/Player.java
@@ -10,5 +10,21 @@ package Ficha;
  * @author JoseLuis
  */
 public class Player {
-    
+    String user,password;
+    int ranking;
+    public static String LOGGED_USER;
+    public Player(String use,String pass){
+    user=use;
+    password=pass;
+    ranking =0;
+    }
+    public String getUser(){
+    return user;
+    }
+    public int getRank(){
+    return ranking;
+    }
+    public void setLog(String user){
+        LOGGED_USER=user;
+    }
 }
